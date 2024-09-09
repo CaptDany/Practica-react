f73f92693318c066e0c6836190784a3264a84323
luisglz04 <st4069@utr.edu.mx>
2024-09-09 09:50:48 -0600

    se modificó archivo app.js
    se añadió una función para cambiar el color de forma aleatoria cuando se haga click en la pantalla

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "./components/Image";
 import Text from "./components/Text";
 
-function App() {
+const App = () => {
+  const [bgColor, setBgColor] = useState("#ffffff");
+  const [refreshTrigger, setRefreshTrigger] = useState(0); // Used to trigger a refresh in the child components
+
+  const handlePageClick = () => {
+    setRefreshTrigger(refreshTrigger + 1);
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setBgColor(randomColor);
+  };
+
   return (
-    <>
-      <div>App</div>
-      <Image />
-      <Text />
-    </>
+    <div
+      style={{
+        backgroundColor: bgColor,
+        minHeight: "100vh",
+        textAlign: "center",
+        padding: "50px",
+      }}
+      onClick={handlePageClick}
+    >
+      <Image refreshTrigger={refreshTrigger} />
+      <Text refreshTrigger={refreshTrigger} />
+    </div>
   );
-}
+};
 
 export default App;
